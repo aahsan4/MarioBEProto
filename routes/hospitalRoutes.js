e64ac0992dbee9b8b2229e52e521a73a0d7b0978
DESKTOP-5MV5A5J\Takvir <takvir.alam@selise.ch>
2025-02-09 22:59:52 +0600

    ENV DB

--- routes/hospitalRoutes.js
+++ routes/hospitalRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const hospitalController = require('../controllers/hospitalController');
 
+
+
+
 router.get('/', hospitalController.getAllHospitals);
 router.get('/:id', hospitalController.getHospitalById);
 router.post('/', hospitalController.createHospital);
